92e48e36a8fc57f08c0c11dbe55273e443b10912
luis <luisluislu31@gmail.com>
2015-12-12 17:29:30 -0430

    error con persisted?

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class OmniauthCallbacksController <ApplicationController
 	def facebook
 		auth = request.env["omniauth.auth"]#el hash con la info del usuario se encuentra en omniauth.auth q contiene la informacion para autenticar al usuario
-		#raise auth.to_yaml 
+		#raise auth.to_yaml #explota una excepcion de ruby a proposito para poder inspeccionar el archivo o hash que nos envio omniauth de regreso
 		data = {
 			nombre: auth.info.first_name, #infornacion que requeriremos del usuario
 			apellido: auth.info.last_name,
@@ -11,16 +11,16 @@ class OmniauthCallbacksController <ApplicationController
 			uid: auth.uid #identificador
 		}
 
-		@user = User.find_or_create_by_omniauth(data)
+		@user = User.find_or_create_by_omniauth(data) #hacemos uso del metodo que creamos en el modelo de usuario, pasandole como parametro el hash que lleva por nombre data
 
-		if @user.persisted?
-			sign_in_and_redirect @user, event: :authentication
-		else
-			session[:omniauth_errors] = @user.errors.full_messages.to_sentence unless @user.save
+		if @user.persisted? #el metodo persisted? del active records de rails devuelve verdadero si el registro ya esta guardado dentro de nuestra base de datos. si en este caso la variable usuarios contiene la informacion completa pero no pudo guardarse por alguna razon, persisted devolvera falso.
+			sign_in_and_redirect @user, event: :authentication #en caso de que el usuario si se pudo guardar, lo que haremos sera loggearlo y redireccionarlo
+		else #en caso de que devuelva faso, por algun error como que ese email ya exista en nuestra base de datos
+			session[:omniauth_errors] = @user.errors.full_messages.to_sentence unless @user.save #notificamos al usuario los errores que impidieron que se guardara y .to_sentence convierte ese mensaje en mensaje natural, en vez de codigo
 		 	
-		 	session[:omniauth_data] = data
+		 	session[:omniauth_data] = data #guardamos el hash con la informacion
 
-		 redirect_to new_user_registration_url
+		 redirect_to new_user_registration_url #si no pudimos registrar al usuario, lo enviamos a la ventana de reistro
 		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]   
 
-  def self.find_or_create_by_omniauth(auth)
- 	 user = User.where(provider: auth[:provider], uid: auth[:uid]).first
+  def self.find_or_create_by_omniauth(auth) #ayuda a saber si el usuario se a loggeado antes con omniauth, y va a reibir como parametro el hash data que se encuentra en omniauth_callbacks_controller
+ 	 user = User.where(provider: auth[:provider], uid: auth[:uid]).first #busca en la base de datos un usuario que ya se haya loggeado con esas mismas credenciales, o sea q ya exista un registro dentro de nuestra tabla de usuarios con el uid y el provider para identificar a los usuarios. el provider y el uid los sacamos del hash de omniauth_callbacks_controller
 
-  unless user
+  unless user #aqui ya guardamos nuestra informacion en la base de datos
   	user = User.create(
   		nombre: auth[:nombre],
   		apellido: auth[:apellido],
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   		email: auth[:email],
   		uid: auth[:uid],
   		provider: auth[:provider],
-  		password: Devise.friendly_token[0,20]
+  		password: Devise.friendly_token[0,20] #ya que devise no permite que nosotros agreguemos un registro con el password vacio, entonces generamos una cadena de caracteres aleatoreos ya que no podemos colocar una contraseña estandar porque eso explotaria la seguridad de nuestra aplicacion. friendly_token es un metodo de devise para generar contraseñas aleatorias
 		)
 	end
  end    
