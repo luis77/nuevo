dd32b9b7a89133a268f86eb29256e59a3a20b995
alex <alexmarcano720@gmail.com>
2016-01-04 17:57:05 -0430

    correcciones_acceso_migraciones_post

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,12 @@ ActiveRecord::Schema.define(version: 20151221210050) do
     t.string   "titulo"
     t.text     "contenido"
     t.string   "extension"
-    t.integer  "usuario_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "posts", ["usuario_id"], name: "index_posts_on_usuario_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
