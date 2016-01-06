b6eca50baeafb3d120eab9ea7e73aa9b83cc32c7
alex <alexmarcano720@gmail.com>
2016-01-06 19:03:30 -0430

    frontend perfiles

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
 	def show
+		@user = User.find(params[:id]) #guarda el registro en la variable global id, toma el id de la url, y busca al usuario, almacenandolo en la variable de clase.
 	end
 end
